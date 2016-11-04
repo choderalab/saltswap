3cc936b0cadb734975fb26497c2388bdcbc3cbb3
John Chodera <choderaj@mskcc.org>
2016-11-04 00:05:49 -0400

    Fix bugs where sigma was being modified and epsilon for hydrogens was incorrect

--- Performance/MinimalTest2/minimal_test.py
+++ Performance/MinimalTest2/minimal_test.py
@@ -2,22 +2,25 @@ import time
 from simtk import openmm, unit
 from simtk.openmm import app
 from openmmtools.testsystems import WaterBox
-from simtk.openmm import LangevinIntegrator
+from simtk.openmm import VerletIntegrator, LangevinIntegrator
 from integrators import GHMCIntegrator
 
 platform = openmm.Platform.getPlatformByName('OpenCL')
 properties = {'Precision': 'mixed'}
-box_edge = 20.0 * unit.angstrom
+box_edge = 10.0 * unit.angstrom
 wbox = WaterBox(box_edge=box_edge, cutoff=box_edge / 2.05, nonbondedMethod=app.PME)
 force = wbox.system.getForce(2) # NonbondedForce
 nsteps = 50000 # number of switching steps
 temperature = 300.0 * unit.kelvin
 collision_rate = 1.0 / unit.picoseconds
 timestep = 2.0 * unit.femtoseconds
+
+print('System has %d particles' % wbox.system.getNumParticles())
+
 def set_lambda(context, lambda_value=1.0):
-    force.setParticleParameters(0,charge=-0.834*lambda_value,sigma=0.3150752406575124*lambda_value,epsilon=0.635968*lambda_value)
-    force.setParticleParameters(1,charge=0.417*lambda_value,sigma=0,epsilon=1*lambda_value)
-    force.setParticleParameters(2,charge=0.417*lambda_value,sigma=0,epsilon=1*lambda_value)
+    force.setParticleParameters(0,charge=-0.834*lambda_value,sigma=0.3150752406575124,epsilon=0.635968*lambda_value)
+    force.setParticleParameters(1,charge=0.417*lambda_value,sigma=0.1,epsilon=0.0)
+    force.setParticleParameters(2,charge=0.417*lambda_value,sigma=0.1,epsilon=0.0)
     force.updateParametersInContext(context)
 
 def create_context(integrator):
@@ -28,8 +31,17 @@ def create_context(integrator):
     integrator.step(500)
     return context
 
-print('System has %d particles' % wbox.system.getNumParticles())
 format = '%64s: %8.3f s for %8d steps (%8.3f ps) : %8.3f ms / step : %5.3f x'
+
+# Time LangevinIntegrator without switching
+integrator = LangevinIntegrator(temperature, collision_rate, timestep)
+context = create_context(integrator)
+initial_time = time.time()
+integrator.step(nsteps)
+elapsed_time = time.time() - initial_time
+baseline = elapsed_time
+del context, integrator
+print(format % ('LangevinIntegrator.step(nsteps)', elapsed_time, nsteps, nsteps*timestep/unit.picoseconds, 1000*elapsed_time/float(nsteps), elapsed_time/baseline))
 
 # Time LangevinIntegrator without switching
 integrator = LangevinIntegrator(temperature, collision_rate, timestep)
@@ -38,9 +50,8 @@ initial_time = time.time()
 for step in range(nsteps):
     integrator.step(1)
 elapsed_time = time.time() - initial_time
-baseline = elapsed_time
 del context, integrator
-print(format % ('LangevinIntegrator', elapsed_time, nsteps, nsteps*timestep/unit.picoseconds, 1000*elapsed_time/float(nsteps), elapsed_time/baseline))
+print(format % ('LangevinIntegrator step(1) loop', elapsed_time, nsteps, nsteps*timestep/unit.picoseconds, 1000*elapsed_time/float(nsteps), elapsed_time/baseline))
 
 # Time LangevinIntegrator with switching
 integrator = LangevinIntegrator(temperature, collision_rate, timestep)
